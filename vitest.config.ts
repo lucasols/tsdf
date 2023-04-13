8d8bf73afecdad9bea346b44ee0e0afce494a859
lucaslos <lucas@sugestly.com>
2023-04-13 16:01:08 -0300

    add test timeout back

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     include: ['test/*.test.{ts,tsx}'],
-    // testTimeout: 5_000,
+    testTimeout: 5_000,
     setupFiles: 'test/setup/setup.ts',
     environment: 'happy-dom',
   },
