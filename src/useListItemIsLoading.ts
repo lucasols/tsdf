2699d724debf5500ad2e2bd99e2a59ef55680911
lucaslos <lucas@sugestly.com>
2023-10-11 14:30:27 -0300

    improve useListItemIsLoading

--- src/useListItemIsLoading.ts
+++ src/useListItemIsLoading.ts
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import { useTimeout } from './utils/useTimeout';
+import { useConst, useLatestValue } from './utils/hooks';
 
 /** Use to detect if a item of a list may be loading in cases which there is no
  * clear indication of the status of individual items, ex: an item in a document collection that is loaded on demand */
@@ -7,24 +8,62 @@ export function useListItemIsLoading({
   isLoading,
   isRefetching,
   isNotFound,
+  loadItemFallback,
+  itemId,
 }: {
+  itemId: string;
   isRefetching: boolean;
   isLoading: boolean;
   isNotFound: boolean;
+  loadItemFallback: () => void;
 }): boolean {
   const [willBeRefetched, setWillBeRefetched] = useState(isNotFound);
 
-  const timeout = useTimeout(1000);
+  const resetWillBeRefechedTimeout = useTimeout(1000);
+  const callFallbackLoadItemTimeout = useTimeout(100);
 
   const ignoreSetWillBeRefetched = useRef(false);
+  const itemWasRefetched = useConst(() => new Set<string>());
 
-  if (isNotFound && !willBeRefetched && !ignoreSetWillBeRefetched.current) {
+  if (
+    isNotFound &&
+    !isRefetching &&
+    !willBeRefetched &&
+    !ignoreSetWillBeRefetched.current &&
+    !itemWasRefetched.has(itemId)
+  ) {
     setWillBeRefetched(true);
   }
 
+  const latestIsLoadingOrRefetching = useLatestValue(isLoading || isRefetching);
+  const latestLoadItemFallback = useLatestValue(loadItemFallback);
+
+  useEffect(() => {
+    if (isRefetching) {
+      itemWasRefetched.add(itemId);
+      resetWillBeRefechedTimeout.clear();
+      callFallbackLoadItemTimeout.clear();
+      setWillBeRefetched(false);
+    }
+  }, [
+    callFallbackLoadItemTimeout,
+    isRefetching,
+    itemId,
+    itemWasRefetched,
+    resetWillBeRefechedTimeout,
+  ]);
+
   useEffect(() => {
     if (willBeRefetched) {
-      timeout.call(() => {
+      callFallbackLoadItemTimeout.call(() => {
+        if (!latestIsLoadingOrRefetching.insideEffect) {
+          latestLoadItemFallback.insideEffect();
+
+          return;
+        }
+      });
+
+      resetWillBeRefechedTimeout.call(() => {
         ignoreSetWillBeRefetched.current = true;
         setWillBeRefetched(false);
 
@@ -34,8 +73,17 @@ export function useListItemIsLoading({
       });
     }
 
-    return () => timeout.clear();
-  }, [timeout, willBeRefetched]);
+    return () => {
+      resetWillBeRefechedTimeout.clear();
+      callFallbackLoadItemTimeout.clear();
+    };
+  }, [
+    resetWillBeRefechedTimeout,
+    willBeRefetched,
+    latestIsLoadingOrRefetching,
+    latestLoadItemFallback,
+    callFallbackLoadItemTimeout,
+  ]);
 
   return isLoading || (isNotFound && (isRefetching || !!willBeRefetched));
 }
--- test/useListItemIsLoading.test.ts
+++ test/useListItemIsLoading.test.ts
@@ -1,201 +1,399 @@
 import { act, renderHook } from '@testing-library/react';
-import { expect, test } from 'vitest';
+import { describe, expect, test, vi } from 'vitest';
 import { useListItemIsLoading } from '../src/useListItemIsLoading';
 import { Store } from 't-state';
 import { sleep } from './utils/sleep';
 import { createRenderStore } from './utils/storeUtils';
 
-test('do not return true if not loading', () => {
-  const { result } = renderHook(() => {
-    const isLoadingItem = useListItemIsLoading({
+function emulateSuccesRefetching(
+  storeState: Store<{
+    itemId: string;
+    isLoading: boolean;
+    isNotFound: boolean;
+    isRefetching: boolean;
+  }>,
+) {
+  return async () => {
+    storeState.setPartialState({
+      isLoading: false,
+      isNotFound: true,
+      isRefetching: true,
+    });
+
+    await sleep(100);
+
+    storeState.setPartialState({
       isLoading: false,
       isNotFound: false,
       isRefetching: false,
     });
+  };
+}
 
-    return isLoadingItem;
-  });
+function emulateFailedRefetching(
+  storeState: Store<{
+    itemId: string;
+    isLoading: boolean;
+    isNotFound: boolean;
+    isRefetching: boolean;
+  }>,
+) {
+  return async () => {
+    storeState.setPartialState({
+      isLoading: false,
+      isNotFound: true,
+      isRefetching: true,
+    });
 
-  expect(result.current).toBe(false);
-});
+    await sleep(100);
 
-test('rendering a component that is not found and then refreshing the resource', async () => {
-  const storeState = new Store({
-    state: {
+    storeState.setPartialState({
       isLoading: false,
       isNotFound: true,
       isRefetching: false,
-    },
-  });
-
-  const rendersResult = createRenderStore();
-
-  renderHook(() => {
-    const itemResourceState = storeState.useState();
-
-    const isLoadingItem = useListItemIsLoading(itemResourceState);
-
-    rendersResult.add({
-      isLoadingItem,
-      notFound: itemResourceState.isNotFound && !isLoadingItem,
     });
+  };
+}
+
+describe.concurrent('useListItemIsLoading', () => {
+  test('do not return true if not loading', () => {
+    const { result } = renderHook(() => {
+      const isLoadingItem = useListItemIsLoading({
+        itemId: '1',
+        isLoading: false,
+        isNotFound: false,
+        isRefetching: false,
+        loadItemFallback: () => {},
+      });
+
+      return isLoadingItem;
+    });
+
+    expect(result.current).toBe(false);
   });
 
-  await sleep(10);
+  test('rendering a component that is not found and then refreshing the resource', async () => {
+    const storeState = new Store({
+      state: {
+        itemId: '1',
+        isLoading: false,
+        isNotFound: true,
+        isRefetching: false,
+      },
+    });
 
-  storeState.setState({
-    isLoading: false,
-    isNotFound: true,
-    isRefetching: true,
-  });
+    const rendersResult = createRenderStore();
 
-  await sleep(100);
+    const loadItemFallbackMock = vi.fn();
 
-  act(() => {
+    renderHook(() => {
+      const itemResourceState = storeState.useState();
+
+      const isLoadingItem = useListItemIsLoading({
+        ...itemResourceState,
+        loadItemFallback: loadItemFallbackMock,
+      });
+
+      rendersResult.add({
+        isLoadingItem,
+        notFound: itemResourceState.isNotFound && !isLoadingItem,
+      });
+    });
+
+    await sleep(10);
+
     storeState.setState({
+      itemId: '1',
       isLoading: false,
-      isNotFound: false,
-      isRefetching: false,
+      isNotFound: true,
+      isRefetching: true,
     });
-  });
 
-  expect(rendersResult.snapshot).toMatchInlineSnapshot(`
+    await sleep(100);
+
+    act(() => {
+      storeState.setState({
+        itemId: '1',
+        isLoading: false,
+        isNotFound: false,
+        isRefetching: false,
+      });
+    });
+
+    expect(loadItemFallbackMock).not.toBeCalled();
+
+    expect(rendersResult.snapshot).toMatchInlineSnapshotString(`
     "
+    isLoadingItem: true -- notFound: false
     isLoadingItem: true -- notFound: false
     isLoadingItem: true -- notFound: false
     isLoadingItem: false -- notFound: false
     "
   `);
-});
-
-test('return loading if resource is loading', async () => {
-  const storeState = new Store({
-    state: {
-      isLoading: true,
-      isNotFound: true,
-      isRefetching: false,
-    },
   });
 
-  const rendersResult = createRenderStore();
-
-  renderHook(() => {
-    const itemResourceState = storeState.useState();
-
-    const isLoadingItem = useListItemIsLoading(itemResourceState);
-
-    rendersResult.add({
-      isLoadingItem,
-      notFound: itemResourceState.isNotFound && !isLoadingItem,
+  test('return loading if resource is loading', async () => {
+    const storeState = new Store({
+      state: {
+        itemId: '1',
+        isLoading: true,
+        isNotFound: true,
+        isRefetching: false,
+      },
     });
-  });
 
-  await sleep(100);
+    const rendersResult = createRenderStore();
 
-  act(() => {
-    storeState.setState({
-      isLoading: false,
-      isNotFound: false,
-      isRefetching: false,
+    renderHook(() => {
+      const itemResourceState = storeState.useState();
+
+      const isLoadingItem = useListItemIsLoading({
+        ...itemResourceState,
+        loadItemFallback: emulateSuccesRefetching(storeState),
+      });
+
+      rendersResult.add({
+        isLoadingItem,
+        notFound: itemResourceState.isNotFound && !isLoadingItem,
+      });
     });
-  });
 
-  expect(rendersResult.snapshot).toMatchInlineSnapshot(`
+    await sleep(100);
+
+    act(() => {
+      storeState.setState({
+        itemId: '1',
+        isLoading: false,
+        isNotFound: false,
+        isRefetching: false,
+      });
+    });
+
+    expect(rendersResult.snapshot).toMatchInlineSnapshotString(`
     "
     isLoadingItem: true -- notFound: false
     isLoadingItem: false -- notFound: false
     "
   `);
-});
+  });
 
-test('reset loading if resource is never refetched', async () => {
-  const storeState = new Store({
-    state: {
+  test('retry loading if resource is never refetched', async () => {
+    const storeState = new Store({
+      state: {
+        itemId: '1',
+        isLoading: false,
+        isNotFound: true,
+        isRefetching: false,
+      },
+    });
+
+    const rendersResult = createRenderStore();
+
+    renderHook(() => {
+      const itemResourceState = storeState.useState();
+
+      const isLoadingItem = useListItemIsLoading({
+        ...itemResourceState,
+        loadItemFallback: emulateSuccesRefetching(storeState),
+      });
+
+      rendersResult.add({
+        isLoadingItem,
+        notFound: itemResourceState.isNotFound && !isLoadingItem,
+      });
+    });
+
+    await sleep(1300);
+
+    expect(rendersResult.changesSnapshot).toMatchInlineSnapshotString(`
+    "
+    isLoadingItem: true -- notFound: false
+    isLoadingItem: false -- notFound: false
+    "
+  `);
+  });
+
+  test('load a new item in the component', async () => {
+    const storeState = new Store({
+      state: {
+        itemId: '1',
+        isLoading: false,
+        isNotFound: false,
+        isRefetching: false,
+      },
+    });
+
+    const rendersResult = createRenderStore();
+
+    const loadItemFallbackMock = vi.fn();
+
+    renderHook(() => {
+      const itemResourceState = storeState.useState();
+
+      const isLoadingItem = useListItemIsLoading({
+        ...itemResourceState,
+        loadItemFallback: loadItemFallbackMock,
+      });
+
+      rendersResult.add({
+        itemId: itemResourceState.itemId,
+        isLoadingItem,
+        notFound: itemResourceState.isNotFound && !isLoadingItem,
+      });
+    });
+
+    await sleep(100);
+
+    expect(loadItemFallbackMock).not.toBeCalled();
+
+    // new item is loading
+
+    storeState.setState({
+      itemId: '2',
       isLoading: false,
       isNotFound: true,
       isRefetching: false,
-    },
-  });
-
-  const rendersResult = createRenderStore();
-
-  renderHook(() => {
-    const itemResourceState = storeState.useState();
-
-    const isLoadingItem = useListItemIsLoading(itemResourceState);
-
-    rendersResult.add({
-      isLoadingItem,
-      notFound: itemResourceState.isNotFound && !isLoadingItem,
     });
+
+    storeState.setState({
+      itemId: '2',
+      isLoading: false,
+      isNotFound: true,
+      isRefetching: true,
+    });
+
+    await sleep(100);
+
+    // new item is loaded
+
+    storeState.setState({
+      itemId: '2',
+      isLoading: false,
+      isNotFound: false,
+      isRefetching: false,
+    });
+
+    await sleep(10);
+
+    expect(loadItemFallbackMock).not.toBeCalled();
+
+    expect(rendersResult.snapshot).toMatchInlineSnapshotString(`
+    "
+    itemId: 1 -- isLoadingItem: false -- notFound: false
+    itemId: 2 -- isLoadingItem: true -- notFound: false
+    itemId: 2 -- isLoadingItem: true -- notFound: false
+    itemId: 2 -- isLoadingItem: false -- notFound: false
+    "
+  `);
   });
 
-  await sleep(1100);
+  test('load a new item in the component but fail, external refetch', async () => {
+    const storeState = new Store({
+      state: {
+        itemId: '1',
+        isLoading: false,
+        isNotFound: false,
+        isRefetching: false,
+      },
+    });
 
-  expect(rendersResult.snapshot).toMatchInlineSnapshot(`
+    const rendersResult = createRenderStore();
+
+    const loadItemFallbackMock = vi.fn();
+
+    renderHook(() => {
+      const itemResourceState = storeState.useState();
+
+      const isLoadingItem = useListItemIsLoading({
+        ...itemResourceState,
+        loadItemFallback: loadItemFallbackMock,
+      });
+
+      rendersResult.add({
+        itemId: itemResourceState.itemId,
+        isLoadingItem,
+        notFound: itemResourceState.isNotFound && !isLoadingItem,
+      });
+    });
+
+    await sleep(100);
+
+    // new item is loading
+
+    storeState.setState({
+      itemId: '2',
+      isLoading: false,
+      isNotFound: true,
+      isRefetching: false,
+    });
+
+    storeState.setState({
+      itemId: '2',
+      isLoading: false,
+      isNotFound: true,
+      isRefetching: true,
+    });
+
+    await sleep(100);
+
+    // resource is refetched but not found
+
+    storeState.setState({
+      itemId: '2',
+      isLoading: false,
+      isNotFound: true,
+      isRefetching: false,
+    });
+
+    await sleep(100);
+
+    expect(loadItemFallbackMock).not.toBeCalled();
+
+    expect(rendersResult.snapshot).toMatchInlineSnapshotString(`
     "
+    itemId: 1 -- isLoadingItem: false -- notFound: false
+    itemId: 2 -- isLoadingItem: true -- notFound: false
+    itemId: 2 -- isLoadingItem: true -- notFound: false
+    itemId: 2 -- isLoadingItem: false -- notFound: true
+    "
+  `);
+  });
+
+  test('fail to load not found item with fallback loading', async () => {
+    const storeState = new Store({
+      state: {
+        itemId: '1',
+        isLoading: false,
+        isNotFound: true,
+        isRefetching: false,
+      },
+    });
+
+    const rendersResult = createRenderStore();
+
+    renderHook(() => {
+      const itemResourceState = storeState.useState();
+
+      const isLoadingItem = useListItemIsLoading({
+        ...itemResourceState,
+        loadItemFallback: emulateFailedRefetching(storeState),
+      });
+
+      rendersResult.add({
+        isLoadingItem,
+        notFound: itemResourceState.isNotFound && !isLoadingItem,
+      });
+    });
+
+    await sleep(500);
+
+    expect(rendersResult.snapshot).toMatchInlineSnapshotString(`
+    "
+    isLoadingItem: true -- notFound: false
+    isLoadingItem: true -- notFound: false
     isLoadingItem: true -- notFound: false
     isLoadingItem: false -- notFound: true
     "
   `);
-});
-
-test.concurrent('load a new item in the component', async () => {
-  const storeState = new Store({
-    state: {
-      isLoading: false,
-      isNotFound: false,
-      isRefetching: false,
-    },
   });
-
-  const rendersResult = createRenderStore();
-
-  renderHook(() => {
-    const itemResourceState = storeState.useState();
-
-    const isLoadingItem = useListItemIsLoading(itemResourceState);
-
-    rendersResult.add({
-      isLoadingItem,
-      notFound: itemResourceState.isNotFound && !isLoadingItem,
-    });
-  });
-
-  await sleep(100);
-
-  // new item is loading
-
-  storeState.setState({
-    isLoading: false,
-    isNotFound: true,
-    isRefetching: false,
-  });
-
-  storeState.setState({
-    isLoading: false,
-    isNotFound: true,
-    isRefetching: true,
-  });
-
-  await sleep(100);
-
-  // new item is loaded
-
-  storeState.setState({
-    isLoading: false,
-    isNotFound: false,
-    isRefetching: false,
-  });
-
-  await sleep(10);
-
-  expect(rendersResult.snapshot).toMatchInlineSnapshot(`
-    "
-    isLoadingItem: false -- notFound: false
-    isLoadingItem: true -- notFound: false
-    isLoadingItem: true -- notFound: false
-    isLoadingItem: false -- notFound: false
-    "
-  `);
 });
