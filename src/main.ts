1e2877b5233e3ea52fa074f12d75ca053ed33776
lucasols <email@lucassantos.net>
2026-02-15 03:02:30 -0300

    feat: reorder exports in main module for improved clarity

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 /* eslint-disable @ls-stack/no-reexport -- this is needed for the lib */
-export * from './documentStore';
 export * from './collectionStore/collectionStore';
+export * from './documentStore';
 export * from './listQueryStore/listQueryStore';
 export * from './listQueryStore/types';
-export * from './requestScheduler';
-export * from './utils/storeShared';
 export * from './utils/performMutation';
+export * from './utils/storeShared';
