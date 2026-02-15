18aff440a03d19b8357af138fe821194bab009f2
lucasols <email@lucassantos.net>
2026-02-15 03:01:56 -0300

    feat: add main module exports for stores, hooks, utilities, and types

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @ls-stack/no-reexport -- this is needed for the lib */
+export * from './documentStore';
+export * from './collectionStore/collectionStore';
+export * from './listQueryStore/listQueryStore';
+export * from './listQueryStore/types';
+export * from './requestScheduler';
+export * from './utils/storeShared';
+export * from './utils/performMutation';
