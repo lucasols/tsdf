8ca733f41513340e072e05675e435f3c8cc5199c
lucasols <email@lucassantos.net>
2026-02-15 03:04:06 -0300

    feat: reorganize exports in main module for better structure and clarity

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,68 @@
 /* eslint-disable @ls-stack/no-reexport -- this is needed for the lib */
-export * from './collectionStore/collectionStore';
+// Store creation functions
+export { createCollectionStore } from './collectionStore/collectionStore';
+export { createDocumentStore } from './documentStore';
+export { createListQueryStore } from './listQueryStore/listQueryStore';
+
+export { fetchTypePriority, StoreFetchError } from './utils/storeShared';
+
+// Document Store types
 export * from './documentStore';
-export * from './listQueryStore/listQueryStore';
-export * from './listQueryStore/types';
-export * from './utils/performMutation';
-export * from './utils/storeShared';
+
+// Collection Store types
+export * from './collectionStore/collectionStore';
+
+// List Query Store types
+export type {
+  ListQueryStore,
+  ListQueryStoreEvents,
+  ListQueryStoreOptions,
+} from './listQueryStore/listQueryStore';
+
+export type {
+  FetchListFnReturn,
+  FetchListFnReturnItem,
+  FieldsInput,
+  FieldsOption,
+  ListQueryStoreInitialData,
+  ListQueryUseMultipleItemsQuery,
+  ListQueryUseMultipleListQueriesQuery,
+  OffsetPaginationConfig,
+  OnListQueryInvalidate,
+  OnListQueryItemInvalidate,
+  OptimisticListUpdate,
+  PartialResourcesConfig,
+  QueryFetchPayload,
+  QueryStatus,
+  TSDFItemQuery,
+  TSFDListQuery,
+  TSFDListQueryState,
+  TSFDUseListItemReturn,
+  TSFDUseListQueryReturn,
+} from './listQueryStore/types';
+
+// Request Scheduler types
+export type {
+  BatchRequest,
+  FetchContext,
+  FetchType,
+  PendingRequest,
+  RequestSchedulerEvents,
+  RequestSchedulerOptions,
+  ScheduleFetchOptions,
+  ScheduleFetchResults,
+} from './requestScheduler';
+
+// Shared types
+export type {
+  StoreError,
+  TSDFStatus,
+  ValidPayload,
+  ValidStoreState,
+} from './utils/storeShared';
+
+// Mutation types
+export type {
+  BlockWindowCloseHandler,
+  MutationDebounce,
+} from './utils/performMutation';
